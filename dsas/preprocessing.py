24f6a03e59c9c34048a13d9581a10b2de9849fdc
Lloyd Palum <lpalum@gmail.com>
2021-02-07 13:29:32 -0500

    cleanup to focus on notebooks

--- dsas/preprocessing.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from pyspark.sql.functions import mean, col
-
-ss = StandardScaler()
-ohe = OneHotEncoder(sparse=False, drop=None, handle_unknown='ignore')
-
